f4271959658f3f6ed9c01b96af890bd2e8e7cee6
choopachuck <choopachuck@gmail.com>
2024-12-12 15:14:17 +0300

    add icons

--- src/shared/ui/icon/constants.ts
+++ src/shared/ui/icon/constants.ts
@@ -1,35 +1,46 @@
-import balance from './assets/balance.svg';
-import goal from './assets/goal.svg';
-import home from './assets/home.svg';
-import templates from './assets/templates.svg';
+import about from './assets/about.svg';
 import alarmClock from './assets/alarm-clock.svg';
-import moon from './assets/moon.svg';
-import refresh from './assets/refresh.svg';
-import search from './assets/search.svg';
-import trash from './assets/trash.svg';
+import balance from './assets/balance.svg';
 import bell from './assets/bell.svg';
-import flag from './assets/flag.svg';
+import bug from './assets/bug.svg';
+import calendar from './assets/calendar.svg';
 import chevronDown from './assets/chevron-down.svg';
 import doubleChevronLeft from './assets/double-chevron-left.svg';
-import tick from './assets/tick.svg';
-import calendar from './assets/calendar.svg';
-import plus from './assets/plus.svg';
-import watch from './assets/watch.svg';
-import medal from './assets/medal.svg';
-import triangleDanger from './assets/triangle-danger.svg';
+import flag from './assets/flag.svg';
+import gear from './assets/gear.svg';
+import goal from './assets/goal.svg';
 import help from './assets/help.svg';
+import home from './assets/home.svg';
+import logout from './assets/logout.svg';
+import medal from './assets/medal.svg';
+import moon from './assets/moon.svg';
 import pin from './assets/pin.svg';
-
+import plus from './assets/plus.svg';
+import refresh from './assets/refresh.svg';
+import search from './assets/search.svg';
+import subscriptionActive from './assets/subscription-active-check.svg';
+import telegram from './assets/telegram.svg';
+import templates from './assets/templates.svg';
+import tick from './assets/tick.svg';
+import trash from './assets/trash.svg';
+import triangleDanger from './assets/triangle-danger.svg';
+import watch from './assets/watch.svg';
 export const ICONS = {
 	balance,
 	goal,
+	logout,
+	telegram,
 	home,
 	templates,
+	about,
 	tick,
 	'alarm-clock': alarmClock,
 	moon,
+	gear,
+	bug,
 	refresh,
 	search,
+	subscriptionActive,
 	trash,
 	bell,
 	flag,
