15bdf61ac17ae14b9b80fec021f04abf6de49726
choopachuck <choopachuck@gmail.com>
2024-12-12 10:28:40 +0300

    remove toast

--- src/shared/ui/toast/CloseIcon.tsx
+++
@@ -1,6 +0,0 @@
-import IconComponent from './assets/closeToastIcon.svg';
-import { Props } from './types';
-
-export function CloseIcon(props: Props) {
-	return <IconComponent {...props} />;
-}
--- src/shared/ui/toast/SuccessIcon.tsx
+++
@@ -1,6 +0,0 @@
-import IconComponent from './assets/successToastIcon.svg';
-import { Props } from './types';
-
-export function SuccessIcon(props: Props) {
-	return <IconComponent {...props} />;
-}
