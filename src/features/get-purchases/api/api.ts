3ed9320e2fd8187e5e65b950866af0ff298ca929
choopachuck <choopachuck@gmail.com>
2025-01-13 09:37:30 +0300

    not last

--- src/features/get-purchases/api/api.ts
+++ src/features/get-purchases/api/api.ts
@@ -4,4 +4,5 @@ import { Purchase } from './types';
 export const getPurchases = async (customerId: string) => {
 	const res = await httpService.lib.get<Purchase[]>(`/v2/purchase/${customerId}`);
 	return res.data.sort((a, b) => new Date(parseInt(b.createdAt)).getTime() - new Date(parseInt(a.createdAt)).getTime());
+	return res.data;
 };
