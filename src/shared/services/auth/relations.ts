f39ad733d8c117281f1624d53da887f6ea10660d
Andrei Efremenkov <efremenkovan99@gmail.com>
2025-01-17 17:57:44 +0100

    add auth token refresh

--- src/shared/services/auth/relations.ts
+++ src/shared/services/auth/relations.ts
@@ -72,7 +72,17 @@ onIdTokenChanged(auth, async (user) => {
 		return;
 	}
 
-	const idToken = (await user.getIdToken()) ?? null;
+	const idTokenResult = await user.getIdTokenResult();
+	const idToken = idTokenResult.token ?? null;
+	const expiresIn = new Date(idTokenResult.expirationTime).getTime() - Date.now();
+	console.log('idToken', idToken);
+	setTimeout(
+		() => {
+			// Force update token on expiration
+			user.getIdTokenResult(true);
+		},
+		expiresIn - 1000 * 60
+	);
 	dispatchEvent(httpService.inputs.setHeader, { key: 'Authorization', value: `Bearer ${idToken}` });
 });
 
