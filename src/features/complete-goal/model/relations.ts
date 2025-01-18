1ec0003227f35c9297a9fe009b264df02ad43b1e
Andrei Efremenkov <efremenkovan99@gmail.com>
2025-01-18 21:11:35 +0100

    fix all found bugs, add progress displaying

--- src/features/complete-goal/model/relations.ts
+++ src/features/complete-goal/model/relations.ts
@@ -30,3 +30,30 @@ sample({
 		remote: internals.updateGoalCompletionFx
 	})
 });
+
+sample({
+	clock: inputs.completeSimpleGoal,
+	source: { goalsAndAchievements: goalEntity.outputs.$goalsAndAchievements },
+	filter: ({ goalsAndAchievements }, { goalId }) => {
+		return goalsAndAchievements.some(({ id }) => id === goalId);
+	},
+	fn: ({ goalsAndAchievements }, { goalId }) => {
+		const goal = goalsAndAchievements.find(({ id }) => id === goalId)!;
+		const newGoalState = goalEntity.lib.completeSimpleGoal(goal);
+
+		return {
+			local: {
+				goalId,
+				goal: newGoalState
+			},
+			remote: {
+				goalId,
+				goal: mapGoalToDTO(newGoalState)
+			}
+		};
+	},
+	target: spread({
+		local: goalEntity.inputs.updateGoal,
+		remote: internals.updateGoalCompletionFx
+	})
+});
--- src/widgets/overdue-goals-list/container.tsx
+++ src/widgets/overdue-goals-list/container.tsx
@@ -1,37 +1,56 @@
 import { GoalCard, goalEntity } from '&entities/goal';
+import { completeGoalFeature } from '&features/complete-goal';
+import { EditGoalFormSidebar } from '&features/edit-goal';
 import { timeService } from '&shared/services/time';
-import { Button } from '&shared/ui/button';
 import { Icon } from '&shared/ui/icon';
 import { Typography } from '&shared/ui/typography';
 import { cn } from '&shared/utils';
 import { useUnit } from 'effector-react';
+import React from 'react';
+import { inputs, outputs } from './model';
 import { Props } from './types';
 
 export function OverdueGoalListWidget({ className, ...attributes }: Props) {
-	const { overdueGoals, realTimestamp } = useUnit({
+	const {
+		overdueGoals,
+		realTimestamp,
+		setSelectedGoalIdEvent,
+		resetSelectedGoalIdEvent,
+		selectedGoalId,
+
+		fillComplexGoalProgressEvent,
+		completeSimpleGoalEvent
+	} = useUnit({
 		overdueGoals: goalEntity.outputs.$overdueGoals,
-		realTimestamp: timeService.outputs.$realTimestamp
+		realTimestamp: timeService.outputs.$realTimestamp,
+		selectedGoalId: outputs.$selectedGoalId,
+		setSelectedGoalIdEvent: inputs.setSelectedGoalId,
+		resetSelectedGoalIdEvent: inputs.resetSelectedGoalId,
+
+		fillComplexGoalProgressEvent: completeGoalFeature.inputs.fillComplexGoalProgress,
+		completeSimpleGoalEvent: completeGoalFeature.inputs.completeSimpleGoal
 	});
+
+	const handleCloseEditForm = React.useCallback(() => {
+		resetSelectedGoalIdEvent();
+	}, []);
 
 	if (overdueGoals.length === 0) {
 		return null;
 	}
 
 	return (
-		<section className={cn('overflow-y-scroll no-scrollbar pb-6', className)} {...attributes}>
-			<div className="px-4 py-6 rounded-2xl border-1 border border-color-gray-10 grow-1 min-h-full">
+		<section className={cn('no-scrollbar overflow-y-scroll pb-6', className)} {...attributes}>
+			<div className="border-1 grow-1 min-h-full rounded-2xl border border-color-gray-10 px-4 py-6">
 				<div className="flex items-center justify-between">
 					<div className="flex items-center gap-2">
-						<Icon name="triangle-danger" className="text-color-error w-6 h-6" />
+						<Icon name="triangle-danger" className="h-6 w-6 text-color-error" />
 						<Typography variant="heading-4" className="font-semibold">
 							Просроченные
 						</Typography>
 					</div>
-					<Button variant="icon" appearance="primary" className="w-8 h-8">
-						<Icon name="plus" className="w-4 h-4 text-color-white" />
-					</Button>
 				</div>
-				<div className="flex flex-col gap-4 mt-6">
+				<div className="mt-6 flex flex-col gap-4">
 					{overdueGoals.map((goal) => {
 						return (
 							<GoalCard
@@ -45,11 +64,27 @@ export function OverdueGoalListWidget({ className, ...attributes }: Props) {
 										? timeService.lib.getDiffInTimeUnits(goal.targetDate, realTimestamp)
 										: null
 								}
+								onClick={() => setSelectedGoalIdEvent(goal.id)}
 							/>
 						);
 					})}
 				</div>
 			</div>
+
+			{selectedGoalId && (
+				<EditGoalFormSidebar
+					isOpen={Boolean(selectedGoalId)}
+					onClose={handleCloseEditForm}
+					goalId={selectedGoalId}
+					onFillComplexGoalProgress={(delta) =>
+						fillComplexGoalProgressEvent({
+							goalId: selectedGoalId,
+							progressDelta: delta
+						})
+					}
+					onCompleteSimpleGoal={() => completeSimpleGoalEvent({ goalId: selectedGoalId })}
+				/>
+			)}
 		</section>
 	);
 }
