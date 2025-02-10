ba4bcfa078ac72f9f091e25111c879595ce6d6b6
choopachuck <choopachuck@gmail.com>
2025-02-10 15:59:59 +0300

    add components

--- src/shared/ui/radio-group/component.tsx
+++ src/shared/ui/radio-group/component.tsx
@@ -22,13 +22,13 @@ const RadioGroupItem = React.forwardRef<
 		<RadioGroupPrimitive.Item
 			ref={ref}
 			className={cn(
-				'aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+				'focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border border-solid border-color-gray-30 text-color-gray-30 focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
 				className
 			)}
 			{...props}
 		>
 			<RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-				<Circle className="h-3.5 w-3.5 fill-primary" />
+				<Circle className="h-3.5 w-3.5 fill-color-brand-primary-50" />
 			</RadioGroupPrimitive.Indicator>
 		</RadioGroupPrimitive.Item>
 	);
