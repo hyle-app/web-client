12bd88899321c0203bf4bd64550ed115a6cac43f
choopachuck <choopachuck@gmail.com>
2025-03-03 15:24:52 +0300

    add dark ver of logo

--- src/shared/ui/logo/component.tsx
+++ src/shared/ui/logo/component.tsx
@@ -1,6 +1,12 @@
+import { useTheme } from '../theme-provider';
+import LogoDarkComponent from './assets/logo-dark.svg';
 import LogoComponent from './assets/logo.svg';
 import { Props } from './types';
 
 export function Logo(props: Props) {
+	const { theme } = useTheme();
+	if (theme === 'dark') {
+		return <LogoDarkComponent {...props} />;
+	}
 	return <LogoComponent {...props} />;
 }
