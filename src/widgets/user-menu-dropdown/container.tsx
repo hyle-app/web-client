31c05506f7206a00eb0384faf7720681e9c287af
choopachuck <choopachuck@gmail.com>
2025-03-03 15:20:43 +0300

    add dark theme, add proper icons

--- src/widgets/user-menu-dropdown/container.tsx
+++ src/widgets/user-menu-dropdown/container.tsx
@@ -1,5 +1,6 @@
 import { HttpModeToggle } from '&features/toggle-http-mode';
 import { authService } from '&shared/services/auth';
+import { Button } from '&shared/ui/button';
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -13,10 +14,11 @@ import {
 	DropdownMenuSubTrigger,
 	DropdownMenuTrigger
 } from '&shared/ui/dropdown-menu';
-import { Icon } from '&shared/ui/icon';
+import { Typography } from '&shared/ui/typography';
 import { cn } from '&shared/utils';
 import { Link, useRouter } from '@tanstack/react-router';
 import { useUnit } from 'effector-react';
+import { Bug, Check, ChevronDown, HelpCircle, Home, Info, LogOut, Send, Settings, Trash } from 'lucide-react';
 import { useState } from 'react';
 
 export function UserMenuDropdownWidget() {
@@ -38,20 +40,21 @@ export function UserMenuDropdownWidget() {
 			{isLoggedIn && (
 				<DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
 					<DropdownMenuTrigger>
-						<span className="bg-transparent p-0">
-							<div className="flex items-center gap-2">
-								{user && user.email}
-								<Icon name={'chevron-down'} className={cn(isOpen && 'rotate-180', 'transition-transform')} />
-							</div>
-						</span>
+						<Button appearance="ghost" className="flex items-center gap-2 bg-transparent p-0">
+							<Typography className="font-semibold">{user && user.email}</Typography>
+							<ChevronDown
+								size={24}
+								className={cn({ 'rotate-180': isOpen }, 'text-color-text-and-icon-80 transition-transform')}
+							/>
+						</Button>
 					</DropdownMenuTrigger>
-					<DropdownMenuContent className="w-80 rounded-2xl bg-color-bg-100">
+					<DropdownMenuContent className="w-80 rounded-2xl border-none bg-color-bg-100">
 						<DropdownMenuLabel>
 							<div className="flex w-full flex-col p-2">
-								<span className="text-default text-color-gray-80">Подписка</span>
+								<span className="text-color-gray-80">Подписка</span>
 								<div className="flex flex-row items-center gap-2 text-default">
-									<Icon name="subscriptionActive" />
-									<span className="font-default text-default"> Активна до 20.09.25</span>
+									<Check size={16} className="text-color-success" />
+									<Typography variant="paragraph"> Активна до 20.09.25</Typography>
 								</div>
 							</div>
 						</DropdownMenuLabel>
@@ -59,23 +62,25 @@ export function UserMenuDropdownWidget() {
 						<DropdownMenuGroup>
 							<DropdownMenuItem>
 								<div className="flex flex-row items-center gap-2">
-									<Icon name="home" />
-									<a href="/profile">Личный кабинет</a>
+									<Home size={16} className="text-color-text-and-icon-80" />
+									<a className="text-color-text-and-icon-80" href="/profile">
+										Личный кабинет
+									</a>
 								</div>
 							</DropdownMenuItem>
 							<DropdownMenuSub>
 								<DropdownMenuSubTrigger>
 									<div className="flex flex-row items-center gap-2">
-										<Icon name="gear" />
-										<span>Настройки</span>
+										<Settings size={16} className="text-color-text-and-icon-80" />
+										<span className="text-color-text-and-icon-80">Настройки</span>
 									</div>
 								</DropdownMenuSubTrigger>
 								<DropdownMenuPortal>
-									<DropdownMenuSubContent>
+									<DropdownMenuSubContent className="border-none">
 										<DropdownMenuItem>
 											<div className="flex flex-row items-center gap-2">
-												<Icon name="trash" />
-												<span>Удалить аккаунт</span>
+												<Trash size={16} className="text-color-text-and-icon-80" />
+												<span className="text-color-text-and-icon-80">Удалить аккаунт</span>
 											</div>
 										</DropdownMenuItem>
 									</DropdownMenuSubContent>
@@ -86,8 +91,8 @@ export function UserMenuDropdownWidget() {
 						<DropdownMenuGroup>
 							<DropdownMenuItem>
 								<div className="flex flex-row items-center gap-2">
-									<Icon name="bug" />
-									<a target="_blank" href="https://t.me/Hyle_app_bot" className="text-default">
+									<Bug size={16} className="text-color-text-and-icon-80" />
+									<a target="_blank" href="https://t.me/Hyle_app_bot" className="text-color-text-and-icon-80">
 										Связаться с разработчиками
 									</a>
 								</div>
@@ -95,23 +100,25 @@ export function UserMenuDropdownWidget() {
 							<DropdownMenuItem>
 								<Link to="" className="text-default">
 									<div className="flex flex-row items-center gap-2">
-										<Icon name="help" className="text-color-text-and-icon-80" />
-										<span>Режим обучения</span>
+										<HelpCircle size={16} className="text-color-text-and-icon-80" />
+										<span className="text-color-text-and-icon-80">Режим обучения</span>
 									</div>
 								</Link>
 							</DropdownMenuItem>
 							<DropdownMenuItem>
 								<Link to="" className="text-default">
 									<div className="flex flex-row items-center gap-2">
-										<Icon name="about" />
-										<Link href="/about">О приложении</Link>
+										<Info size={16} className="text-color-text-and-icon-80" />
+										<Link className="text-color-text-and-icon-80" href="/about">
+											О приложении
+										</Link>
 									</div>
 								</Link>
 							</DropdownMenuItem>
 							<DropdownMenuItem>
 								<div className="flex flex-row items-center gap-2">
-									<Icon name="telegram" />
-									<a href="https://t.me/hyle_app" target="_blank" className="text-default">
+									<Send size={16} className="text-color-text-and-icon-80" />
+									<a href="https://t.me/hyle_app" target="_blank" className="text-color-text-and-icon-80">
 										Телеграмм канал
 									</a>
 								</div>
@@ -123,8 +130,9 @@ export function UserMenuDropdownWidget() {
 						<DropdownMenuSeparator />
 						<DropdownMenuItem onClick={onlogout}>
 							<div className="flex flex-row items-center gap-2">
-								<Icon name="logout" />
-								<span>Выйти из аккаунта</span>
+								{/* <Icon name="logout" className="text-color-text-and-icon-80" /> */}
+								<LogOut size={16} className="text-color-text-and-icon-80" />
+								<span className="text-color-text-and-icon-80">Выйти из аккаунта</span>
 							</div>
 						</DropdownMenuItem>
 					</DropdownMenuContent>
