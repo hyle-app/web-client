6fcec4b34095124ac6de1b459fb9dc64029caf34
choopachuck <choopachuck@gmail.com>
2025-03-03 15:26:37 +0300

    add theme toggle, add proper icon

--- src/pages/_auth.tsx
+++ src/pages/_auth.tsx
@@ -1,6 +1,5 @@
 import { CheerUpSidebar } from '&features/cheer-up';
 import { DialogContainer } from '&shared/services/dialog';
-import { Icon } from '&shared/ui/icon';
 import { ThemeToogle } from '&shared/ui/theme-toogle';
 import { cn } from '&shared/utils';
 import { SearchButtonWidget } from '&widgets/search';
@@ -8,6 +7,7 @@ import { inputs, outputs, SideMenu } from '&widgets/side-menu';
 import { UserMenuDropdownWidget } from '&widgets/user-menu-dropdown';
 import { createFileRoute, Outlet, useRouter, useRouterState } from '@tanstack/react-router';
 import { useUnit } from 'effector-react';
+import { Bell } from 'lucide-react';
 import React from 'react';
 
 export const Route = createFileRoute('/_auth')({
@@ -47,12 +47,12 @@ function AuthLayout() {
 				<h1 className="text-heading-2 font-heading-2 text-color-text-and-icon-80">{title}</h1>
 				<span className="flex items-center">
 					<SearchButtonWidget />
-					{/* TODO: Use notifications button widget here */}
-					<span className="flex h-14 w-14 items-center justify-center">
-						<Icon name="bell" className="h-6 w-6" />
-					</span>
 					<span className="flex h-14 w-14 items-center justify-center">
 						<ThemeToogle />
+					</span>
+					{/* TODO: Use notifications button widget here */}
+					<span className="flex h-14 w-14 items-center justify-center">
+						<Bell size={24} className="text-color-text-and-icon-80" />
 					</span>
 					<UserMenuDropdownWidget />
 				</span>
