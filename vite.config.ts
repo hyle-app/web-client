5d608a46a21d7798e7f2944632235d749c3841e8
choopachuck <choopachuck@gmail.com>
2025-03-17 16:01:26 +0300

    disable caching in vite and nginx

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
@@ -18,6 +19,35 @@ export default defineConfig({
 		TanStackRouterVite({
 			routesDirectory: './src/pages',
 			routeFileIgnorePattern: '.(model|constants|types|relations).tsx?'
+		}),
+		createHtmlPlugin({
+			minify: true,
+			inject: {
+				data: {
+					metaTags: `
+            <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate, max-age=0">
+            <meta http-equiv="Pragma" content="no-cache">
+            <meta http-equiv="Expires" content="0">
+          `
+				},
+				tags: [
+					{
+						tag: 'meta',
+						attrs: { 'http-equiv': 'Cache-Control', content: 'no-store, no-cache, must-revalidate, max-age=0' },
+						injectTo: 'head'
+					},
+					{
+						tag: 'meta',
+						attrs: { 'http-equiv': 'Pragma', content: 'no-cache' },
+						injectTo: 'head'
+					},
+					{
+						tag: 'meta',
+						attrs: { 'http-equiv': 'Expires', content: '0' },
+						injectTo: 'head'
+					}
+				]
+			}
 		})
 	],
 	resolve: {
