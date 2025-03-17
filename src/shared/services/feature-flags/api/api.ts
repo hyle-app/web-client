944df30acfc80c8325f787bd3cbd819961419e4b
choopachuck <choopachuck@gmail.com>
2025-03-17 15:39:01 +0300

    pass all users

--- src/shared/services/feature-flags/api/api.ts
+++ src/shared/services/feature-flags/api/api.ts
@@ -4,6 +4,10 @@ import { FetchUserFeatureStateParams, WebBetaResponse } from './types';
 async function fetchUserFeaturesState({
 	customerId
 }: FetchUserFeatureStateParams): Promise<Record<string, WebBetaResponse>> {
+	return {
+		WEB_BETA: 'AVAILABLE'
+	};
+	//TODO: remove after testing
 	const webVersionRes = await httpService.lib.get<WebBetaResponse>(`/v2/customer/${customerId}/offer-status/WEB_BETA`);
 
 	return {
